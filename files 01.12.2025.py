caa19db185cb5fb8b238858fdd9126f93f230530
m2500457-sudo <m2500457@learnatmbro.co.uk>
2025-12-02 16:50:47 +0000

    ok

--- files 01.12.2025.py
+++ files 01.12.2025.py
@@ -1,20 +1,49 @@
-name = input("Please enter your name")
-score = input("Please enter your score?")
-with open("python.txt","a") as file:
-    file.write(f" {name}: {score}\n")
+def inputs():
 
-file_path = "python.txt"
+    name = input("Please enter your name")
+    score = input("Please enter your score?")
+    while True:
+        if score.isdigit():
+            return name, score
+        else:
+            score = input("Please enter your score?")
+def file(name,score):
 
-try:
+    with open("python.txt","a") as file:
+        file.write(f" {name}: {score}\n")
+
+    file_path = "python.txt"
+
+    try:
+        with open(file_path, "r") as file:
+            file_content = file.read()
+            print(file_content, end="")
+    except FileNotFoundError:
+        print(f"Error: The file {file_path} was not found.")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+def search():
+    search_string = input("What name would you like to search?")
+    file_path = "python.txt"
+
     with open(file_path, "r") as file:
-        file_content = file.read()
-        print(file_content, end="")
-except FileNotFoundError:
-    print(f"Error: The file {file_path} was not found.")
-except Exception as e:
-    print(f"An error occurred: {e}")
+        content = file.read()
+        if search_string in content:
+            print("The name" ,search_string, "was found in the file")
+        else:
+            print("The name" ,search_string, "was not found in the file.")
+
+def average():
+    with open(file_path)
 
 
 
-    
-    
+
+
+
+
+tmep = inputs()
+#inputs = ("alex", 24)   
+file(tmep[0], tmep[1])
+search()
