e25a56536cf6a803f907ede55af96ccdd369d0e6
m2500457-sudo <m2500457@learnatmbro.co.uk>
2026-03-04 16:06:05 +0000

    Continuation of Programming Project

--- Programming Project 03.03.2026.py
+++ Programming Project 03.03.2026.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import math
+import datetime
+import numpy as np
 
 df = pd.read_csv(r"C:\Users\M2500457\OneDrive - Middlesbrough College\Documents\GitHub\DSD-Y1-Python\Game_Shop_Sales_300_Rows.csv")
 
@@ -40,14 +42,53 @@ def most_purchased_genre():
 
 def average_price_per_genre():
 
-    average_per_genre = df.groupby("Category")["Units Sold"].mean()
-    
-    average_per_genre.plot(kind="bar")
+    avg_price = df.groupby("Category")["Price"].mean()
+
+    plt.figure(figsize=(8,5))
+    avg_price.plot(kind="line", marker="o")
+
     plt.title("Average Price per Genre")
     plt.xlabel("Genres")
     plt.ylabel("Price")
     plt.xticks(rotation=45)
     plt.tight_layout()
-    plt.show()
-average_price_per_genre()
+    plt.show()    
+#average_price_per_genre()
 
+def start():
+
+    x = datetime.datetime.now()
+
+    print("Hi! The date and time currently is," ,x,) 
+    print(f"{"Main Menu":*^60}")
+    print(f"{"1) Most Purchased Game (Bar Chart)":*^60}")
+    print(f"{"2) Most Purchased Genre (Pie Chart)":*^60}")
+    print(f"{"3)Average Price Per Genre (Line Chart)":*^60}")
+    print(f"{"3) Average Price Per Genre (Line Chart)":*^60}")
+
+
+    while True:
+        try:
+            choice = int(input("Enter a number between 1 and 3: "))
+            
+            if 1 <= choice <= 3:
+                break
+            else:
+                print("Number must be between 1 and 3.")
+
+        except ValueError:
+            print("Invalid input. Please enter a number.")
+
+    if choice == 1:
+        print("You entered choice 1, loading...")
+        most_purchased_game()
+    elif choice == 2:
+        print("You entered choice 2, loading...")
+        most_purchased_genre()
+    elif choice == 3:
+        print("You entered choice 3, loading...")
+        average_price_per_genre()
+    else:
+        print("")
+
+start()
