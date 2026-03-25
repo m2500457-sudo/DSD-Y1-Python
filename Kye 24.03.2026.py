2e6248d31b336cd8e6fc383160221334d89b64de
m2500457-sudo <m2500457@learnatmbro.co.uk>
2026-03-25 15:45:22 +0000

    25.03.2026

--- Kye 24.03.2026.py
+++ Kye 24.03.2026.py
@@ -1,13 +1,32 @@
-
-
-while True:
-    try:
-        parcel_code = int(input("Input parcel code"))
-
-        if lenght == 11:
-            break
+def parcel_code():
+    while True:
+        code = input("Input parcel code: ")
+        
+        if len(code) == 7 and code.isdigit():
+            return code
         else:
-            print("Please enter 11 digits.")
-    except ValueError:
-        print("Please enter your name using digits.")
-return parcel_code
+            print("Please enter a 7-digit number")
+
+def applying_pattern(code):
+    first_six = code[:6]
+    check_digit = int(code[6])
+
+    digits = [int(d) for d in first_six]
+
+    total = 0
+    for i in range(6):
+        total += digits[i] * (i + 1)
+
+    calculated_digit = total % 10
+
+    if calculated_digit == check_digit:
+        return True, "Valid parcel code."
+    else:
+        return False, "Invalid parcel code."
+
+def main():
+    code = parcel_code()
+    result, message = applying_pattern(code)
+    print(message)
+
+main()
